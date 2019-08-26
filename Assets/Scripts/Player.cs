26f669309975505a3ccba3178fcd8ec670884c1d
MrTwister242 <dog22sun@yahoo.com>
2019-08-26 20:21:37 +0200

    Added rigidbodies to player and enemy Revised (unpolished) movement system similar to Astro

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,17 +6,54 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
 
-    void Update()
+    [SerializeField] [Range(0, 100)] public float movementSpeedIncrement = 50;
+    [SerializeField] [Range(0, 100)] public float rotationSpeedIncrement = 50;
+    [SerializeField] [Range(0, 100)] public float maximumRotationSpeed = 200;
+    private float currentRotationSpeed;
+
+    private void Start()
+    {
+        currentRotationSpeed = 0f;
+    }
+
+    private void Update()
     {
         Move();
     }
 
     private void Move()
     {
-        var deltaX = Input.GetAxis("Horizontal");
-        var deltaY = Input.GetAxis("Vertical");
-        var newXPos = transform.position.x + deltaX;
-        var newYPos = transform.position.y + deltaY;
-        transform.position = new Vector2(newXPos, newYPos);
+        //var deltaX = Input.GetAxis("Horizontal");
+        //var deltaY = Input.GetAxis("Vertical");
+        //var newXPos = transform.position.x + deltaX;
+        //var newYPos = transform.position.y + deltaY;
+        //transform.position = new Vector2(newXPos, newYPos);
+
+
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            gameObject.GetComponent<Rigidbody2D>().AddRelativeForce(new Vector2(0, movementSpeedIncrement));
+            currentRotationSpeed = 0f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            gameObject.GetComponent<Rigidbody2D>().AddRelativeForce(new Vector2(0, -movementSpeedIncrement));
+            currentRotationSpeed = 0f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            Mathf.Clamp(currentRotationSpeed += rotationSpeedIncrement, -maximumRotationSpeed, maximumRotationSpeed);
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            Mathf.Clamp(currentRotationSpeed -= rotationSpeedIncrement, -maximumRotationSpeed, maximumRotationSpeed);
+        }
+
+        transform.Rotate(new Vector3(0, 0, 1) * Time.deltaTime * currentRotationSpeed);
+
     }
 }
