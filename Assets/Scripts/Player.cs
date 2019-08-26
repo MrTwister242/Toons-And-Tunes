8f459f1a00d2eff460d2da36699248b26a7a7ddd
MrTwister242 <dog22sun@yahoo.com>
2019-08-26 17:34:20 +0200

    Added background, player, enemy, canvas and camera prefabs. Basic player script to handle horizontal and vertical movement. Project settings (input, screen size)

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        var deltaX = Input.GetAxis("Horizontal");
+        var deltaY = Input.GetAxis("Vertical");
+        var newXPos = transform.position.x + deltaX;
+        var newYPos = transform.position.y + deltaY;
+        transform.position = new Vector2(newXPos, newYPos);
+    }
+}
