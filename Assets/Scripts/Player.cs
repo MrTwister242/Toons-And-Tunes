c9ed8df30d25fb906f13bc8824f34dab00d4ad0c
MrTwister242 <dog22sun@yahoo.com>
2019-08-27 01:09:18 +0200

    Decreased ship size Improved movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,45 +2,56 @@
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] [Range(0, 50)] public float movementSpeedIncrement = 25;
-    [SerializeField] [Range(0, 100)] public float rotationSpeedIncrement = 50;
-    [SerializeField] [Range(100, 300)] public float maximumRotationSpeed = 150;
+    [SerializeField] [Range(0, 40)] public float movementSpeedIncrement = 20f;
+    [SerializeField] [Range(0, 10)] public float maximumForwardVelocity = 5f;
+    [SerializeField] [Range(0, 40)] public float rotationSpeedIncrement = 20f;
+    [SerializeField] [Range(40, 80)] public float maximumRotationSpeed = 60f;
     private float currentRotationSpeed;
+    private Rigidbody2D myRigidbody;
 
     private void Start()
     {
         currentRotationSpeed = 0f;
+        myRigidbody = gameObject.GetComponent<Rigidbody2D>();
     }
 
-    private void Update()
+    private void LateUpdate()
     {
         Move();
     }
 
     private void Move()
     {
-        if (Input.GetKeyDown(KeyCode.Z))
+
+        if (Input.GetKey(KeyCode.Z))
         {
-            gameObject.GetComponent<Rigidbody2D>().AddRelativeForce(new Vector2(0, movementSpeedIncrement));
+            myRigidbody.AddRelativeForce(new Vector2(0, movementSpeedIncrement));
+            float velocityX = Mathf.Clamp(myRigidbody.velocity.x, -maximumForwardVelocity, maximumForwardVelocity);
+            float velocityY = Mathf.Clamp(myRigidbody.velocity.y, -maximumForwardVelocity, maximumForwardVelocity);
+            myRigidbody.velocity = new Vector2(velocityX, velocityY);
             currentRotationSpeed = 0f;
         }
 
-        if (Input.GetKeyDown(KeyCode.S))
+        else if (Input.GetKey(KeyCode.S))
         {
-            gameObject.GetComponent<Rigidbody2D>().AddRelativeForce(new Vector2(0, -movementSpeedIncrement));
+            myRigidbody.AddRelativeForce(new Vector2(0, -movementSpeedIncrement));
+            float velocityX = Mathf.Clamp(myRigidbody.velocity.x, -maximumForwardVelocity, maximumForwardVelocity);
+            float velocityY = Mathf.Clamp(myRigidbody.velocity.y, -maximumForwardVelocity, maximumForwardVelocity);
+            myRigidbody.velocity = new Vector2(velocityX, velocityY);
             currentRotationSpeed = 0f;
         }
 
-        if (Input.GetKey(KeyCode.Q))
+        else if (Input.GetKey(KeyCode.Q))
         {
             Mathf.Clamp(currentRotationSpeed += rotationSpeedIncrement, -maximumRotationSpeed, maximumRotationSpeed);
         }
 
-        if (Input.GetKey(KeyCode.D))
+        else if (Input.GetKey(KeyCode.D))
         {
             Mathf.Clamp(currentRotationSpeed -= rotationSpeedIncrement, -maximumRotationSpeed, maximumRotationSpeed);
         }
 
+        
         transform.Rotate(new Vector3(0, 0, 1) * Time.deltaTime * currentRotationSpeed);
     }
 }
