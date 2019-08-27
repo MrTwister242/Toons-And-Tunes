080883bcee4a24276c2260c6b5c4e2c449587c5a
MrTwister242 <dog22sun@yahoo.com>
2019-08-27 14:26:34 +0200

    Asteroid movement (random direction) Initial setup for health and dealing damage

--- Assets/Scripts/Asteroid.cs
+++ Assets/Scripts/Asteroid.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class Asteroid : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // Configuration
+    [SerializeField] [Range(0f, 40f)] public float movementSpeed = 20f;
+
+    // Cache
+    private Rigidbody2D myRigidbody;
+
+    private void Start()
     {
-        
+        myRigidbody = GetComponent<Rigidbody2D>();
+        InitializeMovement();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void InitializeMovement()
     {
-        
+        float rotationZ = Random.Range(0f, 360f);
+        transform.Rotate(new Vector3(0, 0, rotationZ));
+        myRigidbody.AddRelativeForce(transform.up * movementSpeed);
     }
 }
