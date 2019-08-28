667c2e82ea9ceec2c6dde9e69097e6928b5b04ef
MrTwister242 <dog22sun@yahoo.com>
2019-08-28 03:21:20 +0200

    Many small tweaks Create more scenes Scene Loader initial setup Game Session initial setup UI prefabs

---
+++ Assets/Scripts/GameSession.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using UnityEngine;
+
+public class GameSession : MonoBehaviour
+{
+    // Configuration
+    [SerializeField] [Range(1, 5)] int startingLives = 3;
+
+    // State
+    private int currentLives;
+
+    private void Awake()
+    {
+        int numberOfGameSessions = FindObjectsOfType<GameSession>().Length;
+        if (numberOfGameSessions > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    public void Start()
+    {
+        currentLives = startingLives;
+    }
+
+    public int GetCurrentLives()
+    {
+        return currentLives;
+    }
+
+    public void PlayerDies()
+    {
+        currentLives--;
+        if(currentLives <= 0)
+        {
+            GameOver();
+        }
+        else
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    private void GameOver()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    // configuration
+    [SerializeField] [Range(0, 20)] int timeToWait = 4;
+
+    // state
+    private int currentSceneIndex;
+
+    private void Start()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if (currentSceneIndex == 0)
+        {
+            StartCoroutine(WaitForTime());
+        }
+    }
+
+    IEnumerator WaitForTime()
+    {
+        yield return new WaitForSeconds(timeToWait);
+        LoadNextScene();
+    }
+
+    private void LoadNextScene()
+    {
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+}
